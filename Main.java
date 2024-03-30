27a3095fcdb7f856d0bba58d9a6105bd6e066336
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-30 13:30:39 +0530

    Q2_Insert_Data

--- Main.java
+++ Main.java
@@ -1,45 +1,41 @@
 import java.sql.*;
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        String url = "jdbc:mysql://localhost:3306/college";
+    public static void main(String[] args) throws ClassNotFoundException{
+        String url = "jdbc:mysql://localhost:3306/hP";
         String username = "root";
         String Password = "abhijit@123";
-        String query = "select * from employees";
-        try {
+        String query = "Insert Into employees(id,name,Salary) values (3, 'Abhijit',6000.0)";
+        try{
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver Loaded Successfully !!!!");
-        } catch (ClassNotFoundException e) {
+        }catch(ClassNotFoundException e){
             System.out.println(e.getMessage());
 
         }
-        try {
-            Connection con = DriverManager.getConnection(url, username, Password);
-            System.out.println("Connection Establish Successfully");
+        try{
+            Connection con = DriverManager.getConnection(url,username,Password);
+            System.out.println("connection Establish Successfully");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while(rs.next()){
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String job_title = rs.getString("job_title");
-                double salary = rs.getDouble("salary");
-                System.out.println();
-                System.out.println("=============");
-                System.out.println(" Id " + id);
-                System.out.println(" Name " + name);
-                System.out.println(" Job_title " + job_title);
-                System.out.println(" Salary " + salary);
+             int rowsaffected = stmt.executeUpdate(query);
+             if(rowsaffected>0) {
+                 System.out.println("Insert Successfull" + rowsaffected + "roe(s) affected");
+             }else{
+                 System.out.println("Insertion Failed");
+
             }
-            rs.close();
+
+
             stmt.close();
             con.close();
             System.out.println();
             System.out.println("Connection close successfully !!!!!!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
 
-        }
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+
     }
-
-
 }
 
+
+
+    }
